984bf2c30d3b7f612a4269028763b1fd3d0ea273
Lexionas74 <72361814+Lexionas74@users.noreply.github.com>
2022-06-06 20:17:33 +1000

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -11,5 +11,31 @@ class Fun(commands.Cog):
         choices = ("Heads", "Tails")
         rancoin = random.choice(choices)
         await ctx.reply(rancoin)
+
+    @commands.command(aliases=["8ball"], description="Ask the magic 8ball a question!")
+    async def eightball(self, ctx, *, question):
+        responses = [
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes - definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful.",
+        ]
+        await ctx.send(f":8ball: Question: {question}\n:8ball: Answer {random.choice(responses)}")        
 def setup(bot):
     bot.add_cog(Fun(bot))        
