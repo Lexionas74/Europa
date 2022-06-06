2df07eb79a65b5d9956cf17ab352b1136368bfaf
Lexionas74 <72361814+Lexionas74@users.noreply.github.com>
2022-06-06 19:56:11 +1000

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -8,7 +8,7 @@ class Utility(commands.Cog):
         self.launch_time = datetime.utcnow()
     @commands.command()
     async def uptime(self, ctx):
-        delta_uptime = datetime.utcnow() - self.bot.launch_time
+        delta_uptime = datetime.utcnow() - self.launch_time
         hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
         minutes, seconds = divmod(remainder, 60)
         days, hours = divmod(hours, 24)
