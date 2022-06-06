a7fd86292323e9663cb2dd3a64fd4bc890d4cc21
Lexionas74 <72361814+Lexionas74@users.noreply.github.com>
2022-06-06 18:47:46 +1000

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 from nextcord.ext import commands
 from nextcord import Guild, Interaction, Message
 
-bot = commands.Bot(command_prefix="as!")
+bot = commands.bot(command_prefix="as!")
 my_secret = os.environ['TOKEN']
 
 @bot.event
@@ -14,6 +14,31 @@ async def on_ready():
 @bot.slash_command(name="Example", description="Slash command description here!")
 async def example(interaction: Interaction):
 	await interaction.response.send_message("Message you want to send here")
+
+@bot.command()
+@commands.is_owner()
+async def reload(ctx: commands.Context, extension: str):
+    bot.unload_extension(f"cogs.{extension}")
+    bot.load_extension(f"cogs.{extension}")
+    print(f"Unloaded and Reloaded {extension}")
+    await ctx.reply("Reloaded Cog succesfully!")  
+        
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
+        
+@bot.command()
+@commands.is_owner()
+async def load(ctx, extension):
+    bot.load_extension(f"cogs.{extension}")
+    await ctx.reply("Loaded extension!")
+    
+@bot.command()
+@commands.is_owner()
+async def unload(ctx, extension):
+    bot.unload_extension(f"cogs.{extension}")
+    await ctx.reply("Unloaded extension!")
+
 
 @bot.command()
 async def simprate(ctx):
