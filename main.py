b8a856685dd254120520112d7f15558c64eecc74
Lexionas74 <72361814+Lexionas74@users.noreply.github.com>
2022-06-06 08:37:31 +0000

    efe

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ from nextcord import Guild, Interaction, Message
 bot = commands.Bot(command_prefix="as!")
 my_secret = os.environ['TOKEN']
 
-@bot.slash_command()
-async def test(interaction: Interaction):
-	await interaction.response.send_message("haha balls")
+@bot.event
+async def on_ready():
+	print("uwu")
+
+@bot.slash_command(name="Example", description="Slash command description here!")
+async def example(interaction: Interaction):
+	await interaction.response.send_message("Message you want to send here")
 
 bot.run(my_secret)
