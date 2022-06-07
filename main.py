bd246bba22b5f73155879fa60a7ddb0fe1cb6734
Lexionas74 <72361814+Lexionas74@users.noreply.github.com>
2022-06-07 21:40:18 +1000

    yeah

--- main.py
+++ main.py
@@ -8,115 +8,16 @@ import difflib
 import aiosqlite
 from bot import Europa
 
-async def get_prefix(bot: commands.Bot, message: Message):
-    if not message.guild:
-        return commands.when_mentioned_or("e!")(bot, message)
-    try:
-        return commands.when_mentioned_or(
-            *bot.prefix_cache[message.guild.id])(bot, message)
-    except KeyError:
-        cur = await bot.db.execute(
-            "SELECT prefix FROM prefix WHERE guild_id = ?",
-            (message.guild.id, ))
-        data = await cur.fetchall()
-        if data is None or data == []:
-            await bot.db.execute(
-                "INSERT OR IGNORE INTO prefix(guild_id, prefix) VALUES (?,?)",
-                (message.guild.id, "e!"),
-            )
-            await bot.db.commit()
-            bot.prefix_cache[message.guild.id] = []
-            bot.prefix_cache[message.guild.id].append("e!")
-            data = [("e!", )]
-        prefixes = [prefix for i in data for prefix in i]
-        prefixes = sorted(prefixes, key=lambda m: len(m), reverse=True)
-        return commands.when_mentioned_or(*prefixes)(bot, message)
 
-
-bot = commands.Bot(command_prefix=get_prefix, intents=nextcord.Intents.all())
-bot.prefix_cache = {}
-loop = asyncio.get_event_loop()
+bot = commands.Bot(command_prefix="e!", intents=nextcord.Intents.all())
 my_secret = os.environ['TOKEN']
-
-async def Prefix(bot):
-    await bot.wait_until_ready()
-    bot.db = await aiosqlite.connect("prefix.db")
-    await bot.db.execute(
-        "CREATE TABLE IF NOT EXISTS prefix(guild_id int, prefix text)")
-    await bot.db.commit()
-
-async def Economy(bot):
-    await bot.wait_until_ready()
-    bot.economy_db = await aiosqlite.connect("economy.db")
-    await bot.economy_db.execute(
-        "CREATE TABLE IF NOT EXISTS economy(author_id int, wallet int, bank int, PRIMARY KEY (author_id))"
-    )
-    await bot.economy_db.commit()
 
 @bot.event
 async def on_ready():
     await ch_pr()
     print("uwu")
 
-@bot.event
-async def on_guild_join(guild: Guild):
-    bot.prefix_cache[guild.id].append("e!")
-    await bot.db.execute(
-        "INSERT OR IGNORE INTO prefix(guild_id, pref) VALUES (?,?)",
-        (guild.id, "p!"))
-    await bot.db.commit()
 
-@bot.event
-async def on_guild_remove(guild: Guild):
-    del bot.prefix_cache[guild.id]
-    await bot.db.execute("DELETE FROM prefix WHERE guild_id =pre ?",
-                            (guild.id, ))
-    await bot.db.commit()
-
-@bot.group(description="changes the prefix [admin only]", invoke_without_command=True)
-@commands.check_any(commands.has_permissions(administrator=True), commands.is_owner())
-async def prefix(ctx: commands.Context, prefix: str = None):
-    prefixes = await get_prefix(bot, ctx)
-    prefixes.remove('<@981074794986504253> ')
-    if prefix is None:
-        return await ctx.send(embed=nextcord.Embed(
-            title="Server prefixes",
-            description="\n".join([
-                f"{num}. {prefix}"
-                for num, prefix in enumerate(prefixes, start=1)
-            ]),
-        ))
-    if len(
-            prefixes
-    ) == 7:  # do not change the message or the 7, this is done on purpose
-        return await ctx.send("Can have a maximum of 5 prefixes per server")
-    elif prefix in prefixes:
-        return await ctx.send("That's already a prefix")
-    try:
-        bot.prefix_cache[ctx.guild.id].append(prefix)
-    except KeyError:  # this error should never be raised but just in case
-        print(bot.prefix_cache)
-    await bot.db.execute(
-        "INSERT OR IGNORE INTO prefix(guild_id, prefix) VALUES (?,?)",
-        (ctx.guild.id, prefix),
-    )
-    await bot.db.commit()
-    await ctx.send(f"Added ``{prefix}`` to server prefixes")
-
-@prefix.command()
-async def delete(ctx, prefix):
-    prefixes = await get_prefix(bot, ctx)
-    if prefix in prefixes:
-        await bot.db.execute(
-            "DELETE FROM prefix WHERe guild_id = ? AND prefix = ?",
-            (ctx.guild.id, prefix))
-        try:
-            bot.prefix_cache[ctx.guild.id].remove(prefix)
-        except ValueError:
-            pass
-        await ctx.send(f"Done! removed {prefix} from server prefixes")
-    else:
-        return await ctx.send(f"{prefix} isn't a server prefix")
 
 @bot.slash_command(name="example", description="Slash command description here!")
 async def example(interaction: Interaction):
