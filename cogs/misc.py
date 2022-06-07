0ff6481f713c71fa72e313e72fbf8c5678eb6e47
Lexionas74 <72361814+Lexionas74@users.noreply.github.com>
2022-06-07 16:38:42 +1000

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -5,12 +5,7 @@ from nextcord.ext import commands
 class Misc(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
-    @commands.command()
-    async def test(self, ctx):
-        await ctx.send("balls")
-    @commands.command()
-    async def test_(self, ctx):
-        await ctx.send("balls")
+
 
 
 def setup(bot):
